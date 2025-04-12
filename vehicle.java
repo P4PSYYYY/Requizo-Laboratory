4cab562695d8706ca1c2870b498014ab82166866
P4PSYYYY <rafael.requizo11@gmail.com>
2025-04-12 00:21:51 -0700

    add

---
+++ vehicle.java
@@ -0,0 +1,55 @@
+public class vehicle {
+    // Private attributes
+    private String brand;
+    private int speed;
+    private String fuelType;
+
+    // Constructor
+    public vehicle(String brand, int speed, String fuelType) {
+        this.brand = brand;
+        this.speed = speed;
+        this.fuelType = fuelType;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    // Method to display vehicle details
+    public void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Speed: " + speed + " km/h");
+        System.out.println("Fuel Type: " + fuelType);
+    }
+
+    public class car extends vehicle {
+        // Additional attribute for car
+        private int numberOfDoors;
+
+        // Constructor
+        public car(String brand, int speed, String fuelType, int numberOfDoors) {
+            super(brand, speed, fuelType);
+            this.numberOfDoors = numberOfDoors;
+        }
+
+        // Getter for numberOfDoors
+        public int getNumberOfDoors() {
+            return numberOfDoors;
+        }
+
+        // Overriding displayInfo method
+        @Override
+        public void displayInfo() {
+            super.displayInfo();
+            System.out.println("Number of Doors: " + numberOfDoors);
+        }
+    }
+}
